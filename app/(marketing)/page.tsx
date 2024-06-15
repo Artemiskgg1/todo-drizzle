5723e0163d0fef1e27ab5a2d458722a506fc66d4
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-06-15 11:40:32 +0530

    Refactor marketing page layout and styles

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -18,9 +18,9 @@ const textFont = Poppins({
 const MarketingPage = () => {
   return (
     <BackgroundGradientAnimation>
-      <div className="flex h-screen items-center justify-center flex-col">
+      <div className="flex h-screen items-center justify-center flex-col absolute z-50 inset-0 text-white font-bold px-4 pointer-events-none md:text-3xl text-center">
         <div className="flex items-center justify-center flex-col">
-          <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+          <div className="mb-4 flex items-center shadow-sm p-4 from-sky-200 to bg-pink-200  text-fuchsia-700 rounded-full uppercase">
             <Medal className="h-6 w-6 mr-2" />
             No. 1 Task Management
           </div>
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,12 @@
+// import { auth } from "@clerk/nextjs";
+
+const organizationIdPage = () => {
+  // const { userId, orgId } = auth();
+  return (
+    <div>
+      <h1>Organization Id Page</h1>
+    </div>
+  );
+};
+
+export default organizationIdPage;
