74bbcfc6a3470e696f9ea6172ca7a0b11d15f164
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-06-22 20:43:40 +0530

    middlewares and layouts structure

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,34 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/protected"]);
+const isProtectedRoute = createRouteMatcher([
+  "/organization(.*)",
+  "/board(.*)",
+  "/select-org",
+  "/api/cards(.*)",
+]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+export default clerkMiddleware((auth, req, event) => {
+  if (!auth().userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn({ returnBackUrl: req.url });
+  }
+
+  if (auth().userId && !isProtectedRoute(req)) {
+    let path: string = "/select-org";
+
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    }
+    return NextResponse.redirect(new URL(path, req.url));
+  }
+
+  if (
+    auth().userId &&
+    !auth().orgId &&
+    req.nextUrl.pathname !== "/select-org"
+  ) {
+    return NextResponse.redirect(new URL("/select-org", req.url));
+  }
 });
 
 export const config = {
