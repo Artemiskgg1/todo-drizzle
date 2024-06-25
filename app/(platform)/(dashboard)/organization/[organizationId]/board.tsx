d424acf726d85212cc45e5b5c4947f7dfe395239
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-06-25 10:39:50 +0530

    updated

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -0,0 +1,7 @@
+interface BoardProps {
+  organizationId: string;
+}
+
+const Board = () => {
+  return <div>Board title:{board.title}</div>;
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,27 @@
-const organizationIdPage = () => {
-  return <div>Organization Page</div>;
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+  return (
+    <div className="flex flex-col space-y-4">
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a Board Title"
+          className="border-black border p-1"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>Board title:{board.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default organizationIdPage;
+export default OrganizationIdPage;
