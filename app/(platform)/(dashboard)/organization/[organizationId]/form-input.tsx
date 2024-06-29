f9424b44f881fd7b0c4d025b019a4c5ba606ca34
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-06-29 22:56:50 +0530

    refactor: Use FormInput component in form component for better code organization

--- app/(platform)/(dashboard)/organization/[organizationId]/form-input.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form-input.tsx
@@ -1,17 +1,24 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { useFormStatus } from "react-dom";
+
 interface FormInputProps {
   errors?: {
     title?: string[];
   };
 }
 export const FormInput = ({ errors }: FormInputProps) => {
+  const { pending } = useFormStatus();
   return (
     <div>
-      <input
+      <Input
         id="title"
         name="title"
         required
         placeholder="Enter a Board Title"
         className="border-black border p-1"
+        disabled={pending}
       />
       {errors?.title ? (
         <div>
--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import { State, create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
 import { useFormState } from "react-dom";
 import { FormInput } from "./form-input";
+import { FormButton } from "./form-button";
 export const Form = () => {
   const initialState: State = { message: null, errors: {} };
   const [state, dispatch] = useFormState(create, initialState);
   return (
     <form action={dispatch}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={state.errors} />
+        <FormInput errors={state?.errors} />
       </div>
-      <Button type="submit">Submit</Button>
+      <FormButton />
     </form>
   );
 };
