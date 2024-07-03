8db375afce503d4ecc7ba6916ccd8831ecb23d05
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-07-04 00:37:43 +0530

    feat: Add useCardModal hook to CardItem component

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,9 +1,11 @@
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
       <Toaster />
+      <ModalProvider />
       {children}
     </ClerkProvider>
   );
