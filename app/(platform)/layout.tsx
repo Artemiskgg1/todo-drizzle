379d832742ccc49a72d5c9fbc3b20b3b99058f04
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-07-31 22:50:07 +0530

    feat: Import ModalProvider, QueryProvider, and ClerkProvider in layout.tsx

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,3 +1,4 @@
+//
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
 import { ClerkProvider } from "@clerk/nextjs";
