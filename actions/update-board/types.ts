e1669937fcabe0dd9a06639a013b033707367832
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-08-21 22:49:57 +0530

    feat: Remove unused import in update-board/types.ts

--- actions/update-board/types.ts
+++ actions/update-board/types.ts
@@ -3,7 +3,6 @@ import { Board } from "@prisma/client";
 
 import { ActionState } from "@/lib/create-safe-action";
 import { UpdateBoard } from "./schema";
-import Input from "postcss/lib/input";
 
 export type InputType = z.infer<typeof UpdateBoard>;
 export type ReturnType = ActionState<InputType, Board>;
